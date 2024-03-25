44b303a4faca9a8912bb5a2e84c003b70c819684
NgureWangai <ngurewangai@gmail.com>
2024-03-26 00:05:25 +0300

    js2 looping

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function countDown(num) {
+    while (num >=0) {
+        console.log(num);
+        num--;
+    }
+}
+    
+    function writeCards(names, item) {
+        let messages = [];
+        for (let i = 0; i < names.length; i++) {
+            messages.push(`Thank you, ${names[i]}, for the wonderful ${item} gift!`);
+     }
+     return messages;
+    }
